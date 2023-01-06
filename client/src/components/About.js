fb3ce6a41e5893b3174c6871a02e3303066e425f
gabs-v <gabyvazquez8297@gmail.com>
2023-01-05 20:30:25 -0800

    components

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,7 +2,43 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>About</div>
+        <div>
+            <div className='navbar'>
+                <h3>GV</h3>
+                <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+        <div className='aboutSection'>
+            <div className='career'>
+            lurumn ipsum dollem lurumn ipsum dollem 
+            lurumn ipsum dollem lurumn ipsum dollem lurumn ipsum dollem lurumn ipsum dollem 
+            lurumn ipsum dollem lurumn ipsum dollem lurumn ipsum dollem lurumn ipsum dollem 
+            lurumn ipsum dollem lurumn ipsum dollem lurumn ipsum dollem 
+            lurumn ipsum dollem lurumn ipsum dollem 
+            lurumn ipsum dollem lurumn ipsum dollem 
+            lurumn ipsum dollem lurumn ipsum dollem 
+            lurumn ipsum dollem lurumn ipsum dollem 
+            lurumn ipsum dollem lurumn ipsum dollem 
+            lurumn ipsum dollem lurumn ipsum dollem lurumn ipsum dollem lurumn ipsum dollem lurumn ipsum dollem 
+            </div>
+            <div className='casual'>
+            lurumn ipsum dollem lurumn ipsum dollem 
+            lurumn ipsum dollem lurumn ipsum dollem lurumn ipsum dollem lurumn ipsum dollem 
+            lurumn ipsum dollem lurumn ipsum dollem lurumn ipsum dollem lurumn ipsum dollem 
+            lurumn ipsum dollem lurumn ipsum dollem lurumn ipsum dollem 
+            lurumn ipsum dollem lurumn ipsum dollem 
+            lurumn ipsum dollem lurumn ipsum dollem 
+            lurumn ipsum dollem lurumn ipsum dollem 
+            lurumn ipsum dollem lurumn ipsum dollem 
+            lurumn ipsum dollem lurumn ipsum dollem 
+            lurumn ipsum dollem lurumn ipsum dollem lurumn ipsum dollem lurumn ipsum dollem lurumn ipsum dollem 
+            </div>
+        </div>
+    </div>
     )
 }
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../static/css/home.css'
 
 const Home = () => {
   return (
