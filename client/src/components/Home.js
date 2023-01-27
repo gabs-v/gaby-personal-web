1d7b642c7073610b2a5d36821551d50b7bd9f8c2
gabs-v <gabyvazquez8297@gmail.com>
2023-01-26 22:18:54 -0800

    home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../static/css/home.css'
+import picture from '../static/img/Untitled_Artwork.png'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
@@ -19,7 +20,7 @@ const Home = () => {
           <h1> Hey there, <br/> I'm Gaby! </h1>
           <h2> Software <br/> Engineer  </h2>
         </div>
-        <img src={Untitled_Artwork.png} alt='picture of me'/>
+        <img src={picture} alt='picture of me'/>
       </div>
     </div>
   )
